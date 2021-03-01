c738f69c865173c46958d7fe59a4d7839c078a7b
Paulo Queirós <paulogomes.queiros@gmail.com>
2021-03-01 17:28:13 -0300

    finalização do crud da api

--- controllers/apartamentos.js
+++ controllers/apartamentos.js
@@ -1,11 +1,31 @@
 const Apartamentos = require('../models/apartamentos');
 
 module.exports = app => {
-    app.get('/apartamentos', (req, res) => res.send('você está na rota de apartamentos e ta vacilando'));
+
+    app.get('/apartamentos', (req, res) => {
+        Apartamentos.lista(res);
+    });
+
+    app.get('/apartamentos/:id', (req,res) => {
+        const id = parseInt(req.params.id);
+        Apartamentos.buscaPorId(id, res);
+    });
 
     app.post('/apartamentos', (req, res) => {
         const apartamento = req.body;
-
         Apartamentos.adiciona(apartamento, res);
+    });
+
+    app.patch('/apartamentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Apartamentos.altera(id, valores, res)
+    })
+
+    app.delete('/apartamentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Apartamentos.deleta(id, res)
     })
 }
--- models/apartamentos.js
+++ models/apartamentos.js
@@ -1,4 +1,4 @@
-const conexao = require('../infra/conexao')
+const conexao = require('../infra/conexao');
 
 class Apartamentos {
     adiciona(apartamento, res){
@@ -11,6 +11,53 @@ class Apartamentos {
                res.status(201).json(result);
             }
         });
+    }
+
+    lista(res){
+        const sql = 'SELECT * FROM apartamentos';
+        conexao.query(sql, (erro, resultado) => {
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json(resultado);
+            }
+        });
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM apartamentos WHERE IdApt=${id}`
+        conexao.query(sql, (erro, resultado)=> {
+            const apartamento = resultado[0];
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json(apartamento);
+            }
+        })
+    }
+
+    altera(id, valores, res) {   
+        const sql = 'UPDATE apartamentos SET ? WHERE IdApt=?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM apartamentos WHERE IdApt=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
     }
 }
 
