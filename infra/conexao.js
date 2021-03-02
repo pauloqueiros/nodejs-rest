a2c39c98d1672496b33987edb9c82bd465bf78bc
Paulo Queirós <paulogomes.queiros@gmail.com>
2021-03-01 21:13:55 -0300

    conexao fix

--- infra/conexao.js
+++ infra/conexao.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const conexao = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: 3000,
     user: 'root',
     password: '',
     database: 'softhouse'
