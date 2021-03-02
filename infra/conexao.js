1bccb3f1631ae73c657a7d16f3e84378ab8548a9
Paulo Queirós <paulogomes.queiros@gmail.com>
2021-03-01 21:24:56 -0300

    conexao fixed2

--- infra/conexao.js
+++ infra/conexao.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const conexao = mysql.createConnection({
-    host: 'localhost',
-    port: 3000,
-    user: 'root',
-    password: '',
+    host: 'mysql743.umbler.com',
+    port: 3306,
+    user: 'paulogq',
+    password: '84220675p',
     database: 'softhouse'
 });
 
