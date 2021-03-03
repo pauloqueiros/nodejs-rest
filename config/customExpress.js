844ace70c977a609887e692c96ea310edfff010e
Paulo Queirós <paulogomes.queiros@gmail.com>
2021-03-03 11:47:30 -0300

    fix customExpress

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 module.exports = () => {
     const app = express();
     
-    app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
 
